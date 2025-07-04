1fc61083ed5ca481dc71f25956942a064ec918f3
ponnada-sowjanya <ponnadasowjanya4@gmail.com>
2025-07-04 15:36:14 +0530

    Update axios.js

--- notes-app/src/api/axios.js
+++ notes-app/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000", // ✅ remove /api here
+  baseURL: process.env.REACT_APP_API_URL, // ✅ remove /api here
 });
 
 API.interceptors.request.use((config) => {
